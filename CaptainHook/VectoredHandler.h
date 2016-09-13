d421e0498a5727f171206bab312fc91ba0808e8e
shmuelyr <yr.shmuel@gmail.com>
2016-09-13 16:27:35 +0300

    version 2.0 beta

---
+++ CaptainHook/VectoredHandler.h
@@ -0,0 +1,28 @@
+
+#ifndef __VECTOREDHANDLER_H__
+#define __VECTOREDHANDLER_H__
+
+#include <Windows.h>
+#include "Utils.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	typedef struct _VECTOREXCPTION_RESOLVED {
+		addr pfnOriginalFunction;
+		addr pfnHookedFunction;
+
+	} VECTOREXCPTION_RESOLVED, *PVECTOREXCPTION_RESOLVED;
+
+	extern VECTOREXCPTION_RESOLVED g_VectorHandlerChain[256];
+	extern unsigned int g_uiVectorHandlerMaxChainSize;
+	extern unsigned int g_uiVectorHandlerChainSize;
+
+	LONG HardwareBreakPointManager(PEXCEPTION_POINTERS pExceptionInfo);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __VECTOREDHANDLER_H__ */
